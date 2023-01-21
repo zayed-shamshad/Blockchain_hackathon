674d10fa45c0529a21270712fc27e564a8da37ff
Mohammad Zaid Shamshad <shamshad.1@iitj.ac.in>
2023-01-21 18:24:12 +0530

    initial

---
+++ blockchain_prometeo_frontend/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { fileURLToPath, URL } from 'node:url'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+
+    quasar({
+      sassVariables: 'src/quasar-variables.sass'
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
