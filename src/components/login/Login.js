2970da343ef6dd10add858a08563a1bda8d92992
Dev Goel <75497964+Dev-Goel@users.noreply.github.com>
2023-01-22 01:45:11 +0530

    integrated google login using firebase

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import { auth, provider } from "./config";
+import { signInWithPopup } from "firebase/auth"
 import './Login.css';
 import gift from './gift.svg'
 
@@ -6,6 +8,13 @@ const Login = ({setMenu, main}) => {
 
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
+
+    const handleClick =()=>{
+      signInWithPopup(auth, provider).then((data)=>{
+          console.log("Google Login Success");
+          console.log(data);
+      })
+    }
 
     const Enter = (e) => {
         e.preventDefault();
@@ -16,24 +25,28 @@ const Login = ({setMenu, main}) => {
 
     const currentView = () => {
           return (
-            <form>
-              <h2>Institute Reward System</h2>
-              <fieldset>
-                <legend>Log In</legend>
-                <ul>
-                  <li>
-                    <label for="username">Username:</label>
-                    <input type="text" required onChange={(e) => setUserName(e.target.value)}/>
-                  </li>
-                  <li>
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" required onChange={(e) => setPassword(e.target.value)}/>
-                  </li>
-                </ul>
-              </fieldset>
-              <button onClick={(e) => Enter(e)}>Login</button>
-            </form>
-            
+            <div>
+              <form>
+                <h2>Institute Reward System</h2>
+                <fieldset>
+                  <legend>Log In</legend>
+                  <ul>
+                    <li>
+                      <label for="username">Username:</label>
+                      <input type="text" required onChange={(e) => setUserName(e.target.value)}/>
+                    </li>
+                    <li>
+                      <label for="password">Password:</label>
+                      <input type="password" id="password" required onChange={(e) => setPassword(e.target.value)}/>
+                    </li>
+                  </ul>
+                </fieldset>
+                <button onClick={(e) => Enter(e)}>Login</button>
+              </form>
+              <button onClick={handleClick}>
+              Signin with Google
+              </button>
+            </div>
           )
     };
 
